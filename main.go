d0367d0d57273622589330b5b7609f31fb525b2c
Allandt Bik-Elliott <allandt@gmail.com>
2020-06-09 16:26:40 +0100

    try to clarify pointers and references with comments. Probably nonsense when i look at this again

--- main.go
+++ main.go
@@ -25,7 +25,9 @@ func main() {
 		},
 	}
 
+	// convert jim into a memory address and save
 	jimPointer := &jim
+	// run updateName on the memory address of jim
 	jimPointer.updateName("Jimminy")
 	jim.print()
 }
@@ -34,6 +36,8 @@ func (p person) print() {
 	fmt.Printf("%+v", p)
 }
 
+// use the memory address of type person as receiver
 func (pointerToPerson *person) updateName(newFirstName string) {
+	// dereference the memory address back to the value to update its members
 	(*pointerToPerson).firstName = newFirstName
 }
